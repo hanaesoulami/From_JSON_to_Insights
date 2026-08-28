61f68af36eb669d6b321cd2a952b41c354d31f12
Hanae Soulami <74453506+hanaesoulami@users.noreply.github.com>
2026-08-28 17:32:09 +0100

    Change default database name from sparkifydb to studentdb

--- db.py
+++ db.py
@@ -12,9 +12,9 @@ def get_connection():
         return psycopg2.connect(database_url)
 
     host = os.getenv("DB_HOST", "127.0.0.1")
-    dbname = os.getenv("DB_NAME", "sparkifydb")
+    dbname = os.getenv("DB_NAME", "studentdb")
     user = os.getenv("DB_USER", "postgres")
     password = os.getenv("DB_PASSWORD", "admin")
     port = int(os.getenv("DB_PORT", 5432))
-
+    
     return psycopg2.connect(host=host, dbname=dbname, user=user, password=password, port=port)
